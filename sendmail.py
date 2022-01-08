bba3dc39c415b62fd7fe6e4d91727a1a7c6f66f3
nagasaisriya <63009472+nagasaisriya@users.noreply.github.com>
2022-01-08 17:34:31 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -6,7 +6,7 @@ def sendmail(TEXT,email):
     print("sorry we cant process your candidature")
     s = smtplib.SMTP('smtp.gmail.com',587)
     s.starttls()
-    s.login("...", "...")
+    s.login("heartdiseaseprediction2021@gmail.com", "heartdiseaseprediction")
     Message  = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
     s.sendmail("heartdiseaseprediction2021@gmail.com", email, Message)
     s.quit()
